a8fd13509f84c70ffbdde7c767577228025891e5
Ivan Kondzerau <kondzerau.ivan@gmail.com>
2024-04-26 23:49:12 +0300

    Обработка пользовательского ввода

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log("index.js is on!");
+const ipInput = document.querySelector('.search-bar__input');
+const btn = document.querySelector('.search-bar__btn');
+
+btn.addEventListener('click', getData);
+ipInput.addEventListener('keydown', handleKey);
+
+function getData() {
+  // проверка данных
+  fetch(`https://geo.ipify.org/api/v2/country?apiKey=at_YI3SVi53R1KYCzrLGYBsa5tkrU9X1&ipAddress=${ipInput.value}`)
+    .then(response => response.json())
+    .then(console.log);
+}
+
+function handleKey(event) {
+  if (event.key === 'Enter') {
+    getData();
+  }
+}
