0ef46b8ede854a8ca4df4b176cabc8caa81a1119
Ivan Kondzerau <kondzerau.ivan@gmail.com>
2024-04-28 02:03:20 +0300

    Динамическая работа с DOM

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import {validateIp} from './helpers';
 const ipInput = document.querySelector('.search-bar__input');
 const btn = document.querySelector('.search-bar__btn');
 
+const infoIp = document.getElementById('ip');
+const infoLocation = document.getElementById('location');
+const infoTimezone = document.getElementById('timezone');
+const infoIsp = document.getElementById('isp');
+
 btn.addEventListener('click', getData);
 ipInput.addEventListener('keydown', handleKey);
 
@@ -10,7 +15,7 @@ function getData() {
   if(validateIp(ipInput.value)) {
     fetch(`https://geo.ipify.org/api/v2/country?apiKey=at_YI3SVi53R1KYCzrLGYBsa5tkrU9X1&ipAddress=${ipInput.value}`)
     .then(response => response.json())
-    .then(console.log);
+    .then(setInfo);
   }
 }
 
@@ -18,4 +23,11 @@ function handleKey(event) {
   if (event.key === 'Enter') {
     getData();
   }
+}
+
+function setInfo ({ip, location, isp}) {
+  infoIp.innerText = ip;
+  infoLocation.innerText = `${location.country} / ${location.region}`;
+  infoTimezone.innerText = location.timezone;
+  infoIsp.innerText = isp;
 }
